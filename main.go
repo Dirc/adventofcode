8d524278bdf72e7ac5608c4bf7f28cf085f378f1
Dirc <e.e.cornet@gmail.com>
2025-12-06 14:34:15 +0100

    refactor

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 func main() {
 	//day1("day1.txt")
-	day2("input-day2-dev.txt")
-	//day3("day3-input.txt")
-	//day4("day4.txt")
+	//day2("day2-dev.txt")
+	//day3("day3.txt")
+	//day4Part1("day4.txt")
+	//day4Part2("day4.txt")
+	//day5Part1("day5.txt")
+	day5part2("day5.txt")
+
 }
